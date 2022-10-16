b6acfaaa13a0b1802e9837c17d46e3492ef45965
Rama <ramiroquesadaperez@gmail.com>
2022-10-16 03:33:16 -0300

    Visuals finished

---
+++ clock.js
@@ -0,0 +1,19 @@
+// DOM Elements
+let hourEl = document.getElementById("hour");
+let minuteEl = document.getElementById("minute");
+
+// Set up the time
+const updateTime = () => {
+  const currentTime = new Date();
+
+  let currentHour = currentTime.getHours();
+  const currentMinute = currentTime.getMinutes();
+
+  if (currentHour > 12) {
+    currentHour -= 12;
+  }
+  hourEl.textContent = currentHour.toString();
+  minuteEl.textContent = currentMinute.toString().padStart(2, "0");
+};
+setInterval(updateTime, 1000);
+updateTime();
